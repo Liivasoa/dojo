81775a837fb9f6ce7ba30ebbd994b48a5cacd56c
Liivasoa <liva.ravelotahiana@gmail.com>
2026-06-02 17:01:07 +0200

    feat-fibonacci: Refactor fibonacci controler

--- src/test/java/com/liva/dojo/fibonacci/api/FibonacciControlerTest.java
+++ src/test/java/com/liva/dojo/fibonacci/api/FibonacciControlerTest.java
@@ -1,10 +1,12 @@
-package com.liva.dojo.api;
+package com.liva.dojo.fibonacci.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.client.RestTestClient;
+
+import com.liva.dojo.fibonacci.FibonacciControler;
 
 @WebMvcTest(FibonacciControler.class)
 @AutoConfigureRestTestClient
